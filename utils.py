d3d174974da92fff9169424e7851e90cc51c05ff
malakggh <malakggh@gmail.com>
2024-02-13 11:59:30 +0200

    big work

--- utils.py
+++ utils.py
@@ -7,12 +7,16 @@ def get_ticks(bins, data):
                             step=step,
                             stop=np.max(data)+step, dtype=int)
 
-def plot_on_bars(plt, bars):
+def plot_on_bars_hist(plt, bars):
     for bar, count in zip(bars[2], bars[0]):
         plt.text(bar.get_x() + bar.get_width()/2, count, int(count), ha='center', va='bottom')
 
-def config_plot(plt, title, x_label, y_label):
-    plt.grid(True)
+def plot_on_bars(plt, bars):
+    for index, value in enumerate(bars):
+        plt.text(index, value, str(value), ha='center', va='bottom')
+
+def config_plot(plt, title, x_label, y_label, show_grid=True):
+    plt.grid(show_grid)
     plt.tight_layout()
     plt.title(title)
     plt.xlabel(x_label)
